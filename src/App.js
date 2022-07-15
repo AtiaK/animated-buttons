800d9e34b952ff48054de53d2b39188e614bc16c
AtiaK <atiakalwar@gmail.com>
2022-07-15 21:44:03 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './App.css'
+import {L,M,S} from './components/Buttons'
 
 function App() {
+  const [active, setActive] = useState(-1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" onClick={() => setActive(1)}>
+        <L active={active === 1} />
+      </div>
+      <div className="container" onClick={() => setActive(2)}>
+        <M active={active === 2} />
+      </div>
+      <div className="container" onClick={() => setActive(3)}>
+        <S active={active === 3} />
+      </div>
+     
     </div>
   );
 }
